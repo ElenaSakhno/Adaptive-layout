912b10fea85f8c2aed0add6a1951b16c68086041
Elena Sahno <elenasachno8@gmail.com>
2023-04-28 10:15:08 +0200

    landing page is ready

--- js/main.js
+++ js/main.js
@@ -7,3 +7,45 @@ function documentClick(e) {
         document.documentElement.classList.toggle('menu-open')
     }
 }
+
+// ----------------------------------------------------------------------
+
+const animItems = document.querySelectorAll('._anim-items');
+console.log('lena')
+if (animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll);
+    console.log('lena');
+     function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++)  {
+            const animItem = animItems[index]; 
+            const animItemHeigth = animItem.offsetHeight;  
+            console.log(animItemHeigth);
+            console.log('lena1');
+            const animItemOffset = offset(animItem).top;
+            console.log(animItemOffset,'aninItemOffset');
+            const animStart = 4;
+            let animItemPoint = window.innerHeight - animItemHeigth / animStart;
+            if (animItemHeigth > window.innerWidth) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            
+            if (window.pageYOffset > (animItemOffset - animItemPoint) && window.pageYOffset < (animItemOffset + animItemHeigth)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')){
+                    animItem.classList.remove('_active');
+                }                
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+      }
+      setTimeout(() => {
+        animOnScroll();
+      }, 300);
+      
+} 
